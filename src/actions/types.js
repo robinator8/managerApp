693fe4bb8ffbef2988975f64ea7177eeec8428a4
robinator8 <robertcraig38@gmail.com>
2017-10-28 19:31:39 -0400

    edit

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,7 +1,5 @@
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password_changed';
-export const RESET_EMAIL = 'reset_email';
-export const RESET_PASSWORD = 'reset_password';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER = 'login_user';
